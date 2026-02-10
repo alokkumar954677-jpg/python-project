81f09575b2d0574390c9f95710e99d00abd386e5
alokkumar954677-jpg <alokkumar954677@gmail.com>
2026-02-10 20:34:56 +0530

    add

--- dice_🎮game.py
+++ dice_🎮game.py
@@ -1,24 +1,29 @@
 import random
 import pyttsx3
+n=int(input("enter game round number =="))
+player_name=input("enter player name==")
 engine =pyttsx3.init()
-engine.setProperty('rate', 100) 
+engine.setProperty('rate', 80) 
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-player_name=input("enter player name==")
+used_number=set()
 computer_win =0
 you_win=0
 match_draw=0
 invaled=0
-for i in range(5):
+for i in range(n):
     print(f"Round {i+1}")
     computer= random.choice([1,2,3,4,5,6])
     you= int(input("enter🙎 your dice number(1 to 6)="))
     if(you<1 or you>6):
-        print("you🙎 are invalid dice number=",you)
+        print("you🙎 are invaled dice number=",you)
         invaled +=1
         continue
-    # else:
-    #     print()
+    if you in used_number :
+        print("you already to used number =",you)
+        invaled +=1
+        continue
+    used_number.add(you)
     print("💻computer dice number=",computer)
     print("your 🙎dice number =",you)
     if(computer==you):
@@ -30,7 +35,7 @@ for i in range(5):
     else:
         you_win +=1
         print("you are winner 🏆")
-    print ("\n=====final==========score========")
+print ("\n=====final==========score========")
 print("computer_winner\t\t",computer_win)
 print("you are winner\t\t",you_win)
 print("draw\t\t\t",match_draw)
